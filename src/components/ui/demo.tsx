845269f853a157c5f86b1326745a592f4d7fa4cb
clowno7 <clown7g7@gmail.com>
2025-05-21 23:01:09 +0530

    feat: add SignIn component with animated map background and enhanced styling

---
+++ src/components/ui/demo.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignIn from "./travel-connect-signin-1";
+
+function DemoAiAssistatBasic() {
+  return (
+    <div className="w-full min-h-screen">
+      <SignIn />
+    </div>
+  );
+}
+
+export { DemoAiAssistatBasic };
